50f1debd4db8d26e9dbe791992673b17267282a8
Thunder <31209506+wyh2001@users.noreply.github.com>
2021-04-25 15:06:40 +0800

    Create enable-auto-login.bat

---
+++ enable-auto-login.bat
@@ -0,0 +1,21 @@
+@echo off
+echo suda-wifi-login
+echo by @wyh2001
+echo By adding a task to Windows Task Scheduler, this batch file helps you automate suda-wifi-login's process
+pause
+cls
+echo 1.enable
+echo 2.disable
+echo 3.exit
+set /p op=Type option:
+if "%op%"=="1" goto op1
+if "%op%"=="2" goto op2
+if "%op%"=="3" goto op3
+:op1
+cp login.py login.bat config.json %ProgramFiles%\suda-wifi-login
+schtasks.exe /Create /XML auto-login.xml /tn suda-wifi-login
+:op2
+schtasks.exe /delete suda-wifi-login
+rd %ProgramFiles%\suda-wifi-login
+:op3
+exit
