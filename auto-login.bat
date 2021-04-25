407d942d10eb33e681aa54c2be6c43c7be739d6c
Thunder <31209506+wyh2001@users.noreply.github.com>
2021-04-25 21:33:07 +0800

    Update auto-login.bat

--- auto-login.bat
+++ auto-login.bat
@@ -21,6 +21,11 @@ pause
 goto main
 :main
 cls
+if exist "%ProgramFiles%\suda-wifi-login" (
+    echo Status: ON
+) else (
+    echo Status: OFF
+)
 echo 1.Enable
 echo 2.Disable
 echo 3.Exit
@@ -28,14 +33,19 @@ set /p op=Type option:
 if "%op%"=="1" goto op1
 if "%op%"=="2" goto op2
 if "%op%"=="3" goto op3
+goto main
 :op1
+cls
 md "%ProgramFiles%\suda-wifi-login"
-copy .\*.* "%ProgramFiles%\suda-wifi-login"
+copy config.json "%ProgramFiles%\suda-wifi-login"
+copy login.py "%ProgramFiles%\suda-wifi-login"
+copy login.bat "%ProgramFiles%\suda-wifi-login"
 schtasks.exe /Create /XML auto-login.xml /tn suda-wifi-login
 pause
 cls
 goto main
 :op2
+cls
 schtasks.exe /delete /tn "suda-wifi-login" /F
 rmdir "%ProgramFiles%\suda-wifi-login" /S /Q
 pause
